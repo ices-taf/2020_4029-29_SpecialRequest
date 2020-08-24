c670707cd9982fd38100b913dd8e3c8b52274c1e
Colin Millar <colin.millar@ices.dk>
2020-08-24 12:59:15 +0200

    * initial commit with list of stocks

---
+++ bootstrap/stock_info.R
@@ -0,0 +1,13 @@
+#' List of stocks of interest
+#'
+#' List of stock and stock codes, by area with information on
+#' the contact person
+#'
+#' @name stock_info.csv
+#' @format csv file
+#' @tafOriginator ICES
+#' @tafYear 2020
+#' @tafAccess Restricted
+#' @tafSource file
+
+NULL
