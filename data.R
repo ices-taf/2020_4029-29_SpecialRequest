8b0469f991c5c7222fb743cc3a445dd07ced2696
Colin Millar <colin.millar@ices.dk>
2020-08-24 13:39:34 +0200

    produce summary of stocks to compare with request

---
+++ data.R
@@ -0,0 +1,34 @@
+## Preprocess data, write TAF data tables
+
+## Before:
+## After:
+
+library(icesTAF)
+library(dplyr)
+
+mkdir("data")
+
+stocks <- read.taf(taf.data.path("stock_info.csv"))
+
+stock_table <-
+  stocks %>%
+    by(
+      stocks$area,
+      function(x) {
+        x <- unique(x$species)
+        c(x, rep("", 5))[1:5]
+      }
+    ) %>%
+    unclass() %>%
+    do.call(what = 'cbind.data.frame') %>%
+    select(
+      "NORTH SEA",
+      "NORTH WESTERN WATERS",
+      "SOUTH WESTERN WATERS",
+      "BALTIC SEA",
+      "MEDITERRANEAN SEA"
+    )
+
+stock_table
+
+write.taf(stock_table, dir = "data")
