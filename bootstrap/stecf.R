f0edc8f577cf6ae0270bff93f213ce5466382ddf
colinpmillar <colin.millar@ices.dk>
2020-09-24 14:26:05 +0200

    add stecf data files

---
+++ bootstrap/stecf.R
@@ -0,0 +1,13 @@
+#' List of stocks of interest
+#'
+#' List of stock and stock codes, by area with information on
+#' the contact person
+#'
+#' @name stecf
+#' @format csv files
+#' @tafOriginator STECF
+#' @tafYear 2020
+#' @tafAccess Restricted
+#' @tafSource folder
+
+NULL
