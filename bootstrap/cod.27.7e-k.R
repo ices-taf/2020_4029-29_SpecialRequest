6ac9681f2d57b37909e78525aa0afeffca316f86
Colin Millar <colin.millar@ices.dk>
2020-09-29 20:25:41 +0200

    fix cod 7e repo name

--- bootstrap/cod.27.7e-k.R
+++ bootstrap/cod.27.7e-k.R
@@ -10,13 +10,6 @@
 library(icesTAF)
 source(taf.boot.path("..", "utilities_bootstrap.R"))
 
-data <- get_soa_fs("Cod7ek_Finale")
+data <- get_soa_fs("Cod_7ek_WGCSE2020")
 data$stock_code <- "cod.27.7e-k"
 write.taf(data)
-
-cat(
-  "NOTE:",
-  "* check with Marianne that the final assessment is: Cod7ek_Finale",
-  file = "README.md",
-  sep = "\n"
-)
