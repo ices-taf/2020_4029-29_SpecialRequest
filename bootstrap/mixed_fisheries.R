194a2e453f260834bc2040b84202012c44f01d0f
Colin Millar <colin.millar@ices.dk>
2020-09-30 01:07:33 +0200

    add mixed fisheries data

---
+++ bootstrap/mixed_fisheries.R
@@ -0,0 +1,21 @@
+#' Catches by fleet
+#'
+#' List of stock and stock codes, by area with information on
+#' the contact person
+#'
+#' @name mixed_fisheries
+#' @format csv file
+#' @tafOriginator ICES
+#' @tafYear 2020
+#' @tafAccess Restricted
+#' @tafSource script
+
+library(icesTAF)
+taf.library(icesSharePoint)
+
+spgetfile(
+  "Documents/Preliminary documents/bootstrap_initial_data/mixfish/catch.csv",
+  "/admin/Requests",
+  "https://community.ices.dk",
+  destdir = "."
+)
