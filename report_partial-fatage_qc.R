fbe9d1a834652130f6cc1a26c14fcbcb6652dd0b
Colin Millar <colin.millar@ices.dk>
2020-10-01 14:46:02 +0200

    finished partial F QC

--- report_partial-fatage_qc.R
+++ report_partial-fatage_qc.R
@@ -28,47 +28,93 @@ pfdata <-
 pfdata$ftype <- rep(names(tables), sapply(tables, nrow))
 
 stocks <- unique(pfdata$stock_code)
-
-# prop by year, fleet and age
-prop_yfa <-
-  sapply(
-    stocks,
-    function(stock) {
-      pfdata %>%
-        filter(ftype == "fishing_cat" & stock_code == stock) %>%
-        ggplot(aes(x = as.numeric(year), y = nprop, col = fleet_grp)) +
-        geom_line() +
-        geom_point() +
-        facet_wrap(~ stock_code + age)
-    },
-    simplify = FALSE
-  )
+ftypes <- unique(pfdata$ftype)
 
 
-# partial by age, year and fleet
-pfatage_ayf <-
-  sapply(
-    stocks,
-    function(stock) {
-      pfdata %>%
-        filter(ftype == "fishing_cat" & stock_code == stock) %>%
-        ggplot(aes(x = as.numeric(age), y = partial_f, col = factor(year))) +
-        geom_line() +
-        geom_point() +
-        facet_wrap(~ ftype + fleet_grp, scales = "free")
-    },
-    simplify = FALSE
-  )
+plot_lists <- list()
+
+for (type in ftypes) {
+
+  # prop by year, fleet and age
+  prop_yfa <-
+    sapply(
+      stocks,
+      function(stock) {
+        pfdata %>%
+          filter(ftype == type & stock_code == stock) %>%
+          ggplot(aes(x = as.numeric(year), y = nprop, col = fleet_grp)) +
+          geom_line() +
+          geom_point() +
+          facet_wrap(~ stock_code + age)
+      },
+      simplify = FALSE
+    )
 
 
+  pfatage_yfa <-
+    sapply(
+      stocks,
+      function(stock) {
+        pfdata %>%
+          filter(ftype == type & stock_code == stock) %>%
+          ggplot(aes(x = as.numeric(year), y = partial_f, col = fleet_grp)) +
+          geom_line() +
+          geom_point() +
+          facet_wrap(~ stock_code + age)
+      },
+      simplify = FALSE
+    )
+
+
+  # age, year and fleet
+  prop_ayf <-
+    sapply(
+      stocks,
+      function(stock) {
+        pfdata %>%
+          filter(ftype == type & stock_code == stock) %>%
+          ggplot(aes(x = as.numeric(age), y = nprop, col = factor(year))) +
+          geom_line() +
+          geom_point() +
+          facet_wrap(~ ftype + fleet_grp, scales = "free")
+      },
+      simplify = FALSE
+    )
+
+
+  pfatage_ayf <-
+    sapply(
+      stocks,
+      function(stock) {
+        pfdata %>%
+          filter(ftype == type & stock_code == stock) %>%
+          ggplot(aes(x = as.numeric(age), y = partial_f, col = factor(year))) +
+          geom_line() +
+          geom_point() +
+          facet_wrap(~ ftype + fleet_grp, scales = "free")
+      },
+      simplify = FALSE
+    )
+
+  plot_lists[[type]] <-
+    list(
+      prop_yfa = prop_yfa,
+      pfatage_yfa = pfatage_yfa,
+      prop_ayf = prop_ayf,
+      pfatage_ayf = pfatage_ayf
+    )
+}
 
 rmarkdown::render("report_partial-fatage_qc.Rmd", output_file = "qc_partial-fatage", output_dir = "report")
 
-# edit markdown html to remove ghost images
+# edit markdown html to remove ghost images (wait for system to catch up)
+Sys.sleep(2)
 lines <- readLines("report/qc_partial-fatage.html")
 remlines <- grep("^<p><img src=\"data:image/png;base64", lines)
-cat(
-  lines[-remlines],
-  file = "report/qc_partial-fatage.html",
-  sep = "\n"
-)
+if (length(remlines)) {
+  cat(
+    lines[-remlines],
+    file = "report/qc_partial-fatage.html",
+    sep = "\n"
+  )
+}
