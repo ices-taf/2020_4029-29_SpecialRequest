2f4411013fd7b485af818489f1ffb714dd5c6403
Colin Millar <colin.millar@ices.dk>
2020-10-02 15:15:51 +0200

    very tentative northern hake
    likely to be incorrect

--- bootstrap/hke.27.3a46-8abd.R
+++ bootstrap/hke.27.3a46-8abd.R
@@ -6,3 +6,13 @@
 #' @tafYear 2020
 #' @tafAccess Public
 #' @tafSource script
+
+library(icesTAF)
+
+fdata <- read.taf("https://taf.ices.dk/fs/2020_hke.27.3a46-8abd_assessment_alt/output/fatage.csv")
+
+data <- taf2long(fdata, c("year", "age", "harvest"))
+data$stock_code <- "hke.27.3a46-8abd"
+data$assessment_year <- 2020
+
+write.taf(data)
